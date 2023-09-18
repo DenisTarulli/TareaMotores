1863d8af1f5e156d016758f5270e2edad7540b31
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2023-09-18 20:25:24 -0300

    Update Coins.cs

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -8,11 +8,11 @@ public class Coins : MonoBehaviour
     [SerializeField] GameManager gameManager;
     [SerializeField] SoundEffectPlayer soundPlayer;
     private const string IS_PLAYER = "Player";
-    private const string IS_GAMECONTROLLER = "Player";
+    //private const string IS_GAMECONTROLLER = "Player";
     
     private void Start()
     {
-        gameManager = GameObject.FindGameObjectWithTag(IS_GAMECONTROLLER).GetComponent<GameManager>();
+        //gameManager = GameObject.FindGameObjectWithTag(IS_GAMECONTROLLER).GetComponent<GameManager>();
     }
     private void OnTriggerEnter(Collider other)
     {       
